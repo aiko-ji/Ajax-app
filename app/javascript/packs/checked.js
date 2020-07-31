d0faf94ea4c692a353e30d9b8eb6066e7dc2e64a
aiko-ji <68524938+aiko-ji@users.noreply.github.com>
2020-07-31 15:25:12 +0900

    レスポンスに対応する記述完了

--- app/javascript/packs/checked.js
+++ app/javascript/packs/checked.js
@@ -3,7 +3,22 @@ function check() {
   postsA = Array.from(posts);
 
   postsA.forEach(function (post) {
-    post.addEventListener("click", (e) => { });
+    post.addEventListener("click", (e) => {
+      const postId = post.getAttribute("data-id");
+      const XHR = new XMLHttpRequest();
+      XHR.open("GET", `/posts/${postId}`, true);
+      XHR.responseType = "json";
+      XHR.send();
+      XHR.onload = () => {
+        const item = XHR.response.post;
+        if (item.checked === true) {
+          post.setAttribute("data-check", "true");
+        } else if (item.checked === false) {
+          post.removeAttribute("data-check");
+        }
+      }
+    });
   });
 }
+
 window.addEventListener("load", check);
